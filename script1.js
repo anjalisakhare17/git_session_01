c441d380acbc4cb903d8867a4074ba9cdf37a8ba
salve0609 <salve0609@gmail.com>
2024-02-03 22:17:00 +0530

    added

---
+++ script1.js
@@ -0,0 +1,35 @@
+// Step 1
+// Download and install git 
+// https://git-scm.com/download/win
+
+// Step 2
+// visual studio code 
+// Download and install visual studio code
+// https://code.visualstudio.com/download
+
+// Step 3
+// Github signup
+// https://github.com/signup
+
+// // remember
+// emailaddress
+// username
+// password
+
+// Create a  folder with visual studio code
+// add a file and add the code
+
+// // First two commands to execute only one
+// Set your username: git config --global user.name "username"
+// Set your email address: git config --global user.email "useremail@gmail.com"
+
+// git init
+// git add .
+// git commit -m "login"
+
+// // Sign in to your git hub , Create new repo and run last 3 commands
+// git branch -M main
+// git remote add origin https://github.com/minskolecourse/python.git
+// git push -u origin main
+// // one time username and wil be asked
+
